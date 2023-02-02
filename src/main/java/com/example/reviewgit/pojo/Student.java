cbeab8907b110cb90a590da6c933af8861f01f75
gavin <3138939552@qq.com>
2023-02-02 15:48:15 +0800

    添加内容

--- src/main/java/com/example/reviewgit/pojo/Student.java
+++ src/main/java/com/example/reviewgit/pojo/Student.java
@@ -1,4 +1,53 @@
-package com.example.reviewgit.pojo;/**
+package com.example.reviewgit.pojo;
+
+/**
  * @author Gavin
- */public class Student {
+ */
+public class Student {
+    private String name;
+    private Integer age;
+    private Integer sex;
+    private String idCar;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getIdCar() {
+        return idCar;
+    }
+
+    public void setIdCar(String idCar) {
+        this.idCar = idCar;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", idCar='" + idCar + '\'' +
+                '}';
+    }
 }
